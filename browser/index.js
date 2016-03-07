deab4708b91b11d5d1f8313f4881e0bc93584095
Yusuke Komatsu <tienlen042@gmail.com>
2016-03-07 21:10:37 +0900

    add set_env

--- browser/index.js
+++ browser/index.js
@@ -1,5 +1,6 @@
 var dmm = require('../lib/dmm');
 dmm.request(require('httpplease'));
+dmm.set_env("browser");
 
 module.exports = dmm;
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var dmm = require('./lib/dmm');
 dmm.request(require('request'));
+dmm.set_env("node");
 
 module.exports = dmm;
